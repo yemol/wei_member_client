cdca1bc77aa235f696b014d748e9ed252b2c1b8a
yemol <yemol_yuan@hotmail.com>
2016-11-02 17:09:05 +0800

    Update codes using our service getting openid.

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,25 +2,45 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>{{ code }}</p>
+    <p>{{ state }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  created () {
     const request = require('axios')
-    request.get('https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx5aa86e7885f60a8a&secret=SECRET&code=' + this.$route.query.code + '&grant_type=authorization_code ')
+    request.get('http://service.gamefy.cn/member/userinfo?key=23423fsdjewirwdsk3434eee989&wei=sitv&code=' + this.$route.query.code)
       .then(res => {
-        return {
-          msg: 'Welcome-get',
-          code: this.$route.query.code,
-          state: this.$route.query.state
-        }
+        this.code = res
       })
       .catch(res => {
-        console.log(res)
-        return {msg: 'Welcome-fail'}
+        this.msg = res
       })
+    // this.msg = this.$route.query.code
+  },
+  data () {
+    // const request = require('axios')
+    // request.get('https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx5aa86e7885f60a8a&secret=SECRET&code=' + this.$route.query.code + '&grant_type=authorization_code ')
+    //   .then(res => {
+    //     return {
+    //       msg: this.$route.query.code,
+    //       code: '1',
+    //       state: '2'
+    //     }
+    //   })
+    //   .catch(res => {
+    //     return {
+    //       msg: this.$route.query.code,
+    //       code: '1',
+    //       state: '2'
+    //     }
+    //   })
+    return {
+      msg: '1',
+      code: '2',
+      state: '3'
+    }
   }
 }
 </script>
